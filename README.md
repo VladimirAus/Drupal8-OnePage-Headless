1d4125ed31455b6e0494202b32b55cea77441827
VladimirAus <v_rouakov@yahoo.com>
2015-02-12 00:29:42 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Drupal 8 - One page website - Headless
+Headless website based on web services
+
+### Version
+0.0.1
---
+++ bin/d8.install.with-config.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# General params
+readonly DVER="8.0.0-beta6"
+readonly SITENAME="oplms.d8.loc" 
+readonly DBNAME="loc_d8_oplms"
+
+# MySQL credentials
+readonly DBSERVER="localhost"
+readonly DBLOGIN="root"
+readonly DBPSWD="root"
+
+# Drupal credentials
+readonly DRUPLOGIN="d8admin"
+readonly DRUPPSWD="password"
+readonly DBCONN="mysql://$DBLOGIN:$DBPSWD@$DBSERVER/$DBNAME"
+
+# readonly LMSCONFIG="lms.d8.config"
+
+# Download Drupal
+drush7 dl drupal-8.0.x
+rm -rf docroot
+mv drupal-$DVER docroot
+cd docroot
+
+drush7 si minimal --db-url="$DBCONN" --account-name=$DRUPLOGIN --account-pass=$DRUPPSWD --yes
+# cp -r ../config/$LMSCONFIG sites/default/files
+# mv sites/default/files/$LMSCONFIG sites/default/files/config_$LMSCONFIG
+# chmod -r 775 sites/default/files/config_$LMSCONFIG
+# echo "\$config_directories['$LMSCONFIG'] = 'sites/default/files/config_$LMSCONFIG';" >> sites/default/settings.php
+SITEUUID="uuid: $(drush7 config-get system.site uuid --format=string)"
+
+# cd sites/default/files/
+# CONFIGDIR="$(ls -d c* | head -1)"
+# cd ../../../../config/$LMSCONFIG
+# cp -R . ../../docroot/sites/default/files/$CONFIGDIR/staging/
+
+# cd ../..
+# sed -i.bak "1s/.*/$SITEUUID/" docroot/sites/default/files/$CONFIGDIR/staging/system.site.yml
+
+# cd docroot/
+
+# Following commands do not work in core beta6
+# WORKAROUND: Login to website, enable config module, import staging
+# drush7 en config -y
+# drush7 config-import staging -y
+
