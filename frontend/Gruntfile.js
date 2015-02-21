bb20faa4a6ef52b18c2f92b5343e42d0e26f39eb
VladimirAus <v_roudakov@yahoo.com>
2015-02-21 11:18:48 +1000

    Copy all required files

--- frontend/Gruntfile.js
+++ frontend/Gruntfile.js
@@ -13,22 +13,7 @@ module.exports = function (grunt) {
     config: config,
     pkg: config.pkg,
     bower: grunt.file.readJSON('./.bowerrc'),
-/*    copy: {
-      dist: {
-       files: [{
-         expand: true,
-         cwd: '<%= config.app %>/_lib/font-awesome',
-         src: 'css/font-awesome.min.css',
-         dest: '<%= config.dist %>'
-       },
-       {
-         expand: true,
-         cwd: '<%= config.app %>/_lib/font-awesome',
-         src: 'fonts/*',
-         dest: '<%= config.dist %>'
-       }]
-      }
-    },
+/*    
     uglify: {
       options: {*/
         // banner: '/*! <%= pkg.name %> lib - v<%= pkg.version %> -' +
@@ -46,11 +31,24 @@ module.exports = function (grunt) {
     copy: {
 			main: {
 				files: [
-					// includes files within path and its sub-directories
-					{
+					{ // angular
 						expand: true, 
 						src: ['src/_lib/angular/angular.min.**'], 
 						dest: 'src/lib/angular', 
+						flatten: true, 
+						filter: 'isFile'
+					},
+					{ // jQuery
+						expand: true, 
+						src: ['src/_lib/jquery/dist/jquery.min.**'], 
+						dest: 'src/lib/jquery', 
+						flatten: true, 
+						filter: 'isFile'
+					},
+					{ // foundation
+						expand: true, 
+						src: ['src/_lib/foundation/css/foundation.css**','src/_lib/foundation/js/foundation.min.js'], 
+						dest: 'src/lib/foundation', 
 						flatten: true, 
 						filter: 'isFile'
 					},
